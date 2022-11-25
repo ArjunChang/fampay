209ea69c6fcd896e8f0c643410bcda5ae03284a1
ArjunChang <arjunchang25@gmail.com>
2022-11-25 17:35:59 +0530

    updating restrictions

--- youtube/models.py
+++ youtube/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Video(models.Model):
-    video_id = models.CharField(max_length=15, unique=True)
+    video_id = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    channel = models.CharField(max_length=30)
+    channel = models.CharField(max_length=100)
     published_at = models.DateTimeField()
     thumbnail_url = models.URLField()
 
